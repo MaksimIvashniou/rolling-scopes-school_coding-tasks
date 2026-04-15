c46acc667660a241b89b5f349104e25ea8eadcd5
Maksim Ivashniou <maksim.ivashniou@gmail.com>
2026-04-15 19:08:49 +0300

    feat: implement solution

--- guessing-game/src/index.js
+++ guessing-game/src/index.js
@@ -1,16 +1,25 @@
 class GuessingGame {
   constructor() {
-    throw new Error('Not implemented');
+    this.min = null;
+    this.max = null;
   }
-  /*
-  setRange(min, max) { }
 
-  guess() { }
+  setRange(min, max) {
+    this.min = min;
+    this.max = max;
+  }
 
-  lower() { }
+  guess() {
+    return Math.round((this.max + this.min) / 2);
+  }
 
-  greater() { }
-*/
+  lower() {
+    this.max = this.guess();
+  }
+
+  greater() {
+    this.min = this.guess();
+  }
 }
 
 module.exports = GuessingGame;
