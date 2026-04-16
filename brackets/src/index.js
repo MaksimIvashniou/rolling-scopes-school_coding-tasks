a6867caf9a27209b61187d71ea37f55795b5f943
Maksim Ivashniou <maksim.ivashniou@gmail.com>
2026-04-16 12:19:48 +0300

    feat: implement solution

--- brackets/src/index.js
+++ brackets/src/index.js
@@ -1,3 +1,18 @@
-module.exports = function check(/* str, bracketsConfig */) {
-  throw new Error('Not implemented');
+module.exports = function check(str, bracketsConfig) {
+  const bracketsConfigObj = Object.fromEntries(bracketsConfig);
+  const stack = [];
+
+  for (let i = 0; i < str.length; i += 1) {
+    const bracket = str[i];
+
+    if (bracket === stack[stack.length - 1]) {
+      stack.pop();
+    } else if (bracket in bracketsConfigObj) {
+      stack.push(bracketsConfigObj[bracket]);
+    } else {
+      return false;
+    }
+  }
+
+  return !stack.length;
 };
