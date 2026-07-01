6d83d74459e62f130b4e9c8c8c9816d88e7a0150
Maksim Ivashniou <maksim.ivashniou@gmail.com>
2026-07-01 21:02:09 +0300

    feat: implement solution for task 'Transform array'

--- basics-js/basic-js/src/transform-array.js
+++ basics-js/basic-js/src/transform-array.js
@@ -13,11 +13,60 @@ const { NotImplementedError } = require('../lib');
  * transform([1, 2, 3, '--discard-prev', 4, 5]) => [1, 2, 4, 5]
  *
  */
-function transform(/* arr */) {
-  // Remove line below and write your code here
-  throw new NotImplementedError('Not implemented');
+function transform(arr) {
+  if (!Array.isArray(arr)) {
+    throw new Error("'arr' parameter must be an instance of the Array!");
+  }
+
+  const sequenceEnum = {
+    discardNext: '--discard-next',
+    discardPrev: '--discard-prev',
+    doubleNext: '--double-next',
+    doublePrev: '--double-prev',
+  };
+
+  const copy = [];
+
+  let index = 0;
+
+  while (index < arr.length) {
+    const value = arr[index];
+
+    switch (value) {
+      case sequenceEnum.discardNext: {
+        if (index < arr.length - 1) index += 1;
+        break;
+      }
+
+      case sequenceEnum.doubleNext: {
+        if (index < arr.length - 1) copy.push(arr[index + 1]);
+        break;
+      }
+
+      case sequenceEnum.discardPrev:
+      case sequenceEnum.doublePrev: {
+        if (!index || arr[index - 2] === sequenceEnum.discardNext) break;
+
+        if (value === sequenceEnum.discardPrev) {
+          copy.pop();
+        } else {
+          copy.push(arr[index - 1]);
+        }
+        break;
+      }
+
+      default: {
+        copy.push(value);
+        break;
+      }
+    }
+
+    index += 1;
+  }
+
+  return copy;
 }
 
 module.exports = {
-  transform
+  transform,
 };
