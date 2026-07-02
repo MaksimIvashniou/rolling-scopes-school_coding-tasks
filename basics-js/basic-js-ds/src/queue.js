3f58e262ba0fb12b6ce0644daffda7a4593b8295
Maksim Ivashniou <maksim.ivashniou@gmail.com>
2026-07-02 10:06:12 +0300

    feat: implement solution for task 'Queue'

--- basics-js/basic-js-ds/src/queue.js
+++ basics-js/basic-js-ds/src/queue.js
@@ -1,5 +1,5 @@
 const { NotImplementedError } = require('../lib/errors');
-// const { ListNode } = require('../extensions/list-node.js');
+const { ListNode } = require('../extensions/list-node.js');
 
 /**
  * Implement the Queue with a given interface via linked list (use ListNode extension above).
@@ -13,22 +13,35 @@ const { NotImplementedError } = require('../lib/errors');
  * queue.getUnderlyingList() // returns { value: 3, next: null }
  */
 class Queue {
+  #head = null;
+  #tail = null;
+
   getUnderlyingList() {
-    // Remove line below and write your code here
-    throw new NotImplementedError('Not implemented');
+    return this.#head;
   }
 
-  enqueue(/* value */) {
-    // Remove line below and write your code here
-    throw new NotImplementedError('Not implemented');
+  enqueue(value) {
+    const node = { ...new ListNode(value) };
+
+    if (!this.#tail) {
+      this.#head = node;
+      this.#tail = node;
+      return;
+    }
+
+    this.#tail.next = node;
+    this.#tail = this.#tail.next;
   }
 
   dequeue() {
-    // Remove line below and write your code here
-    throw new NotImplementedError('Not implemented');
+    if (!this.#head) return;
+
+    const { value } = this.#head;
+    this.#head = this.#head.next;
+    return value;
   }
 }
 
 module.exports = {
-  Queue
+  Queue,
 };
